efbb9469a4d4c2b93c9c78edf3feee0d78dd1aa0
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2023-12-28 16:50:51 +0400

    generate token

---
+++ web/myapp/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import CustomUser, Photo
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None) 
+        user = CustomUser.objects.create_user(**validated_data, password=password)
+        return user
+
+class PhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        fields = ('id', 'image')
---
+++ web/myapp/views.py
@@ -0,0 +1,105 @@
+from rest_framework import generics, permissions, status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate, login, get_user_model
+from django.core.mail import send_mail
+from .serializers import UserSerializer, PhotoSerializer
+from django.shortcuts import render
+from .models import Photo
+
+User = get_user_model()
+
+
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+
+            return Response({
+                "user": serializer.data,
+                "access_token": access_token
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'register.html')
+
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(request, email=email, password=password)
+
+        if user:
+            login(request, user)
+
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+
+            return Response({
+                "message": "Login successful",
+                "access_token": access_token
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, 'login.html')
+
+
+class ForgotPasswordView(APIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        user = User.objects.filter(email=email).first()
+
+        if user:
+            reset_token = "generated_reset_token"
+            send_mail('Password Reset', f'Your reset token is: {reset_token}', 'from@example.com', [email])
+            return Response({'message': 'Reset token sent to your email'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class ResetPasswordView(APIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        new_password = request.data.get('new_password')
+
+        user = User.objects.filter(email=email).first()
+        if user:
+            user.set_password(new_password)
+            user.save()
+            return Response({'message': 'Password reset successful'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class PhotoUploadView(generics.CreateAPIView):
+    serializer_class = PhotoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ChangePasswordView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        old_password = request.data.get('old_password')
+        new_password = request.data.get('new_password')
+
+        user = self.request.user
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response({'message': 'Password changed successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid old password'}, status=status.HTTP_401_UNAUTHORIZED)
